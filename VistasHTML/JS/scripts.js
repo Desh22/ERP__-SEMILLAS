4ee84e585debb3c59903fc1ab10b8484144e377f
Desh22 <109133831+Desh22@users.noreply.github.com>
2026-05-14 21:45:09 -0400

    Delete VistasHTML directory

--- VistasHTML/JS/scripts.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("menu-toggle").addEventListener("click", function() {
-    var wrapper = document.getElementById("wrapper");
-    var sidebar = document.getElementById("sidebar-wrapper");
-    sidebar.classList.toggle("toggled");
-});
